abc83bed9876edc877b62ba1292831dd8547bfb3
Anuja Verma <anuja.verma@gmail.com>
2016-03-12 11:08:38 -0800

    Add button to start generations logic

--- app/assets/javascripts/gameBoard.js
+++ app/assets/javascripts/gameBoard.js
@@ -1,16 +1,29 @@
+
 $(document).ready(function () {
+
+  // create a 50X50 grid
   var x = 50;
   var y=50;
   for (var i=0; i < x; i++){
     var row=$('<tr>', {id:"X"+i } ).appendTo('table');
     for(var j=0;j<y;j++){
-      $('<td>',{id:"X"+i+"_"+j, class:"inactive"}).appendTo(row)
+      $('<td>',{id:"X"+i+"_"+j, class:"inactive cell"}).appendTo(row)
     }
   }
 
+  // change td class to activate a TD
   $("td").on("click", function() {
     $(this).toggleClass("inactive active");
   });
 
+  // call startGenerations function on button click
+  $('.start').click(startGenerations);
+
+  // var allElements = document.querySelectorAll(".cell");
+
+
+
+
 });
+
 
---
+++ app/assets/javascripts/generations.js
@@ -0,0 +1,3 @@
+function startGenerations() {
+  console.log("Hi!!")
+  }
