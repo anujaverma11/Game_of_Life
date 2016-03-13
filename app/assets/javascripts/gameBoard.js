3c9c26e97c315b1043f5da176be78d9d06f4dd04
Anuja Verma <anuja.verma@gmail.com>
2016-03-12 19:18:14 -0800

    logic for generations of cell

--- app/assets/javascripts/gameBoard.js
+++ app/assets/javascripts/gameBoard.js
@@ -7,19 +7,18 @@ $(document).ready(function () {
   for (var i=0; i < x; i++){
     var row=$('<tr>', {id:"X"+i } ).appendTo('table');
     for(var j=0;j<y;j++){
-      $('<td>',{id:"X"+i+"_"+j, class:"inactive cell"}).appendTo(row)
+      $('<td>',{id:"X"+i+"_"+j, class:"dead cell"}).appendTo(row)
     }
   }
 
   // change td class to activate a TD
   $("td").on("click", function() {
-    $(this).toggleClass("inactive active");
+    $(this).toggleClass("dead live");
   });
 
   // call startGenerations function on button click
   $('.start').click(startGenerations);
 
-  // var allElements = document.querySelectorAll(".cell");
 
 
 
--- app/assets/javascripts/generations.js
+++ app/assets/javascripts/generations.js
@@ -1,3 +1,41 @@
+// startGenerations function will create generations of the selected cells in a grid.
+// Any live cell with two or three live neighbours lives on to the next generation - this is acheived by cell class property and is not a part of any code change.
+
 function startGenerations() {
-  console.log("Hi!!")
+
+  // fetching values of all cells and assigning it to allElements
+ var cellWillDie = [];
+ var cellWillLive = [];
+ var allElements = document.querySelectorAll(".cell");
+
+  // iterating through all cells to find find next generation live cells
+  for (var i=0, max=allElements.length; i < max; i++) {
+    var liveNeighbours = getLiveNeighbours(allElements, i);
+
+    //Any live cell with fewer than two live neighbours dies,Any live cell with more than three live neighbours dies.
+
+    if ((liveNeighbours<2 || liveNeighbours>3) && allElements[i].className.indexOf("live") > -1){
+      cellWillDie.push(allElements[i].id)
+    }
+
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+    if (liveNeighbours === 3 && allElements[i].className.indexOf("dead") > -1){
+      cellWillLive.push(allElements[i].id)
+    }
   }
+
+  cellWillDie.forEach(function(cellID) {
+    document.getElementById(cellID).className = 'dead cell';
+  });
+  cellWillLive.forEach(function(cellID) {
+    document.getElementById(cellID).className = 'live cell';
+  });
+
+  if (cellWillDie.length > 0 || cellWillLive.length > 0){
+    setTimeout(startGenerations, 500);
+
+  }
+}
+
+
